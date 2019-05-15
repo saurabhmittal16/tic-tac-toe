84dff16b192ceb0913010ac36fd51f43784501e3
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-05-15 20:29:36 +0530

    add state to game and refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'utils.dart';
 
 void main() => runApp(MyApp());
-
-const Color MainColor = Colors.black;
 
 class MyApp extends StatelessWidget {
   @override
@@ -16,24 +15,33 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Tic Tac Toe'),
         ),
-        body: GridView.count(
-          padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-          crossAxisCount: 3,
-          children: List.generate(9, (index) {
-            return Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: MainColor, width: 2),
-              ),
-              child: Center(
-                child: Text(
-                  'X',
-                  style: TextStyle(fontSize: 54, fontWeight: FontWeight.w300),  
-                ),
-              ),
-            );
-          }),
-        ),
+        body: Game()
       ),
+    );
+  }
+}
+
+class Game extends StatefulWidget {
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  List<String> game = ['X','O',' ','X',' ','X','X','O','X'];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      padding: EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+      crossAxisCount: 3,
+      children: List.generate(9, (index) {
+        return GestureDetector(
+          onTap: () {
+            print(game[index]);
+          },
+          child: getTile(index, game[index]),
+        );
+      }),
     );
   }
 }
---
+++ lib/utils.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+Widget getTile (int index, String text) {
+	return Container(
+		decoration: BoxDecoration(
+			border: getBorders(index)
+		),
+		child: Center(
+			child: Text(
+				text,
+				style: TextStyle(fontSize: 64, fontWeight: FontWeight.w400),  
+			),
+		)
+	);
+}
+
+Border getBorders(int index) {
+	switch (index) {
+		case 0: return Border(
+			bottom: BorderSide(width: 2, color: Colors.purple),
+			right: BorderSide(width: 2, color: Colors.purple)
+		);
+		case 1: return Border(
+			bottom: BorderSide(width: 2, color: Colors.purple),
+			left: BorderSide(width: 2, color: Colors.purple),
+			right: BorderSide(width: 2, color: Colors.purple)
+		);
+		case 2: return Border(
+			bottom: BorderSide(width: 2, color: Colors.purple),
+			left: BorderSide(width: 2, color: Colors.purple),
+		);
+		case 3: return Border(
+			top: BorderSide(width: 2, color: Colors.purple),
+			bottom: BorderSide(width: 2, color: Colors.purple),
+			right: BorderSide(width: 2, color: Colors.purple)
+		);
+		case 4: return Border.all(width: 2, color: Colors.purple); 
+		case 5: return Border(
+			top: BorderSide(width: 2, color: Colors.purple),
+			bottom: BorderSide(width: 2, color: Colors.purple),
+			left: BorderSide(width: 2, color: Colors.purple),
+		);
+		case 6: return Border(
+			top: BorderSide(width: 2, color: Colors.purple),
+			right: BorderSide(width: 2, color: Colors.purple)
+		);
+		case 7: return Border(
+			top: BorderSide(width: 2, color: Colors.purple),
+			left: BorderSide(width: 2, color: Colors.purple),
+			right: BorderSide(width: 2, color: Colors.purple)
+		);
+		case 8: return Border(
+			top: BorderSide(width: 2, color: Colors.purple),
+			left: BorderSide(width: 2, color: Colors.purple),
+		);
+	}
+	return Border.all(width: 2, color: Colors.purple);
+}
